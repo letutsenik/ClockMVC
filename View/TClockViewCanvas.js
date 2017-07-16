3c891e522526155469d888b7a7b7597388570a77
DreamerQ <captainhope777@gmail.com>
2017-07-16 18:18:44 +0300

    added ES6 class defenition to canvas view

--- View/TClockViewCanvas.js
+++ View/TClockViewCanvas.js
@@ -1,147 +1,149 @@
-'use strict';
-function TClockViewCanvas () {
-    var ClockModel = null;
-    var ClockDomElem = null;
-    var Canvas = null;
-    var Context = null;
+class TClockViewCanvas {
+    constructor() {
+        this._ClockModel = null;
+        this._ClockDomElem = null;
+        this._Canvas = null;
+        this._Context = null;
 
-    var clockInfo = null;
+        this._clockInfo = null;
 
-    var clockFaceRadius = 250;
-    var clockFaceCenterX = 250;
-    var clockFaceCenterY = 250;
-
-    this.Init = function(Model,Elem) {
-        ClockModel = Model;
-        ClockDomElem = Elem;
-        var info = ClockModel.GetClockInfo();
-
-        Canvas = Elem.querySelector('.canvas');
-        Context = Canvas.getContext('2d');
-        clockInfo = ClockDomElem.querySelector('.info');
-
-        if (info.GMT === 0)clockInfo.innerHTML = info.city + ' ' + '(GMT)';
-        if (info.GMT > 0)clockInfo.innerHTML = info.city + ' ' + '(GMT' + '+' + info.GMT + ')';
-        if (info.GMT < 0)clockInfo.innerHTML = info.city + ' ' + '(GMT' + '-' + -info.GMT + ')';
-    };
-
-    this.Update = function () {
-        render();
-    };
-
-    function drawClockFace() {
-        Context.restore();
-        Context.strokeStyle='#e1bc19';
-        Context.fillStyle='#e1bc19';
-        Context.beginPath();
-        Context.arc(clockFaceCenterX, clockFaceCenterY, clockFaceRadius, 0,Math.PI*2, false);
-        Context.stroke();
-        Context.fill();
+        this._clockFaceRadius = 250;
+        this._clockFaceCenterX = 250;
+        this._clockFaceCenterY = 250;
     }
 
-    function drawNumbers() {
-        var scaleFactor = 0.125; // Маштабный коэффициент для определения размеров кругов с цифрами
-        var scaleRadiusFactor = 0.8; // Маштабный коэффициент для определения радиуса расположения кругов с цифрами
-        var scaleTextSizeFactor = 1.0; // Маштабный коэффициент для определения размера шрифта в кругах с цифрами
-        var scaleTextOffsetFactor = 0.35; //Маштабный коэффициент для определения отступа цифр по вертикали
 
-        var clockNumRadius = Math.round(clockFaceRadius * scaleFactor);
-        var clockNumR = Math.round(clockFaceRadius * scaleRadiusFactor);
-        var textOffset = Math.round(clockNumRadius * scaleTextOffsetFactor);
-        var angelBetweenNum = 30;
+    Init (Model,Elem) {
+        this._ClockModel = Model;
+        this._ClockDomElem = Elem;
+        let info = this._ClockModel.GetClockInfo();
 
-        for (var i = 1; i <= 12; i++) {
-            Context.strokeStyle='#469826';
-            Context.fillStyle='#469826';
-            Context.beginPath();
-            Context.arc(Math.round(clockFaceCenterX + clockNumR * Math.sin(30 * i / 180 * Math.PI)),
-                Math.round(clockFaceCenterY - clockNumR * Math.cos(30 * i / 180 * Math.PI)),
+        this._Canvas = Elem.querySelector('.canvas');
+        this._Context = this._Canvas.getContext('2d');
+        this._clockInfo = this._ClockDomElem.querySelector('.info');
+
+        if (info.GMT === 0) this._clockInfo.innerHTML = info.city + ' ' + '(GMT)';
+        if (info.GMT > 0) this._clockInfo.innerHTML = info.city + ' ' + '(GMT' + '+' + info.GMT + ')';
+        if (info.GMT < 0) this._clockInfo.innerHTML = info.city + ' ' + '(GMT' + '-' + -info.GMT + ')';
+    };
+
+    Update () {
+        this.render();
+    };
+
+    drawClockFace() {
+        this._Context.restore();
+        this._Context.strokeStyle='#e1bc19';
+        this._Context.fillStyle='#e1bc19';
+        this._Context.beginPath();
+        this._Context.arc(this._clockFaceCenterX, this._clockFaceCenterY, this._clockFaceRadius, 0,Math.PI*2, false);
+        this._Context.stroke();
+        this._Context.fill();
+    }
+
+    drawNumbers() {
+        const scaleFactor = 0.125; // Маштабный коэффициент для определения размеров кругов с цифрами
+        const scaleRadiusFactor = 0.8; // Маштабный коэффициент для определения радиуса расположения кругов с цифрами
+        const scaleTextSizeFactor = 1.0; // Маштабный коэффициент для определения размера шрифта в кругах с цифрами
+        const scaleTextOffsetFactor = 0.35; //Маштабный коэффициент для определения отступа цифр по вертикали
+
+        const clockNumRadius = Math.round(this._clockFaceRadius * scaleFactor);
+        const clockNumR = Math.round(this._clockFaceRadius * scaleRadiusFactor);
+        const textOffset = Math.round(clockNumRadius * scaleTextOffsetFactor);
+        const angelBetweenNum = 30;
+
+        for (let i = 1; i <= 12; i++) {
+            this._Context.strokeStyle='#469826';
+            this._Context.fillStyle='#469826';
+            this._Context.beginPath();
+            this._Context.arc(Math.round(this._clockFaceCenterX + clockNumR * Math.sin(30 * i / 180 * Math.PI)),
+                Math.round(this._clockFaceCenterY - clockNumR * Math.cos(30 * i / 180 * Math.PI)),
                 clockNumRadius, 0, Math.PI*2, false);
-            Context.stroke();
-            Context.fill();
+            this._Context.stroke();
+            this._Context.fill();
 
-            Context.fillStyle='black';
-            Context.textAlign='center';
-            Context.font = clockNumRadius * scaleTextSizeFactor + 'px Times New Roman';
-            Context.fillText(i, Math.round(clockFaceCenterX + clockNumR * Math.sin(angelBetweenNum * (i) / 180 * Math.PI)),
-                Math.round(clockFaceCenterY - clockNumR * Math.cos(angelBetweenNum * (i) / 180 * Math.PI)) + textOffset);
+            this._Context.fillStyle='black';
+            this._Context.textAlign='center';
+            this._Context.font = clockNumRadius * scaleTextSizeFactor + 'px Times New Roman';
+            this._Context.fillText(i, Math.round(this._clockFaceCenterX + clockNumR * Math.sin(angelBetweenNum * (i) / 180 * Math.PI)),
+                Math.round(this._clockFaceCenterY - clockNumR * Math.cos(angelBetweenNum * (i) / 180 * Math.PI)) + textOffset);
 
         }
     }
 
-    function drawElectronic() {
-        var now = ClockModel.GetCurrentTime();
-        var OffsetYScaleFactor = 0.77; //Отступ текста по вертикали
-        var hour = now.hours;
+    drawElectronic() {
+        let now = this._ClockModel.GetCurrentTime();
+        const OffsetYScaleFactor = 0.77; //Отступ текста по вертикали
+        let hour = now.hours;
         if (hour < 10 ) hour = '0' + hour;
 
-        var min = now.minutes;
+        let min = now.minutes;
         if (min < 10 ) min = '0' + min;
 
-        var sec = now.seconds;
+        let sec = now.seconds;
         if (sec < 10 ) sec = '0' + sec;
 
-        Context.textAlign='center';
-        Context.font = '40px Times New Roman';
-        Context.fillStyle='#000';
+        this._Context.textAlign='center';
+        this._Context.font = '40px Times New Roman';
+        this._Context.fillStyle='#000';
 
-        Context.fillText(hour + ':' + min + ':' + sec, clockFaceCenterX, clockFaceCenterY * OffsetYScaleFactor);
-        Context.save();
+        this._Context.fillText(hour + ':' + min + ':' + sec, this._clockFaceCenterX, this._clockFaceCenterY * OffsetYScaleFactor);
+        this._Context.save();
     }
 
-    function drawArrows() {
-        var angles = GetArrowsAngel();
-        var hourArrowLenght = 180;
-        var minArrowLenght = 200;
-        var secArrowLenght = 230;
-        var arrowTail = 20;
+    drawArrows() {
+        let angles = this.GetArrowsAngel();
+        const hourArrowLenght = 180;
+        const minArrowLenght = 200;
+        const secArrowLenght = 230;
+        const arrowTail = 20;
 
-        Context.lineCap='round';
+        this._Context.lineCap='round';
 
-        Context.lineWidth = 9.5;
-        Context.strokeStyle='#000';
-        Context.beginPath();
-        Context.moveTo(clockFaceCenterX - arrowTail * Math.sin(angles.hour),
-            clockFaceCenterY + arrowTail * Math.cos(angles.hour));
-        Context.lineTo( clockFaceCenterX + (hourArrowLenght-arrowTail) * Math.sin(angles.hour),
-            clockFaceCenterY - (hourArrowLenght-arrowTail) * Math.cos(angles.hour) );
-        Context.stroke();
+        this._Context.lineWidth = 9.5;
+        this._Context.strokeStyle='#000';
+        this._Context.beginPath();
+        this._Context.moveTo(this._clockFaceCenterX - arrowTail * Math.sin(angles.hour),
+            this._clockFaceCenterY + arrowTail * Math.cos(angles.hour));
+        this._Context.lineTo( this._clockFaceCenterX + (hourArrowLenght-arrowTail) * Math.sin(angles.hour),
+            this._clockFaceCenterY - (hourArrowLenght-arrowTail) * Math.cos(angles.hour) );
+        this._Context.stroke();
 
-        Context.lineWidth = 6.5;
-        Context.strokeStyle='#761010';
-        Context.beginPath();
-        Context.moveTo(clockFaceCenterX - arrowTail * Math.sin(angles.min),
-            clockFaceCenterY + arrowTail * Math.cos(angles.min));
-        Context.lineTo( clockFaceCenterX + (minArrowLenght-arrowTail) * Math.sin(angles.min),
-            clockFaceCenterY - (minArrowLenght-arrowTail) * Math.cos(angles.min) );
-        Context.stroke();
+        this._Context.lineWidth = 6.5;
+        this._Context.strokeStyle='#761010';
+        this._Context.beginPath();
+        this._Context.moveTo(this._clockFaceCenterX - arrowTail * Math.sin(angles.min),
+            this._clockFaceCenterY + arrowTail * Math.cos(angles.min));
+        this._Context.lineTo( this._clockFaceCenterX + (minArrowLenght-arrowTail) * Math.sin(angles.min),
+            this._clockFaceCenterY - (minArrowLenght-arrowTail) * Math.cos(angles.min) );
+        this._Context.stroke();
 
-        Context.lineWidth = 4.5;
-        Context.strokeStyle='#d00a0a';
-        Context.beginPath();
-        Context.moveTo(clockFaceCenterX - arrowTail * Math.sin(angles.sec),
-            clockFaceCenterY + arrowTail * Math.cos(angles.sec));
-        Context.lineTo( clockFaceCenterX + (secArrowLenght-arrowTail) * Math.sin(angles.sec),
-            clockFaceCenterY - (secArrowLenght-arrowTail) * Math.cos(angles.sec) );
-        Context.stroke();
+        this._Context.lineWidth = 4.5;
+        this._Context.strokeStyle='#d00a0a';
+        this._Context.beginPath();
+        this._Context.moveTo(this._clockFaceCenterX - arrowTail * Math.sin(angles.sec),
+            this._clockFaceCenterY + arrowTail * Math.cos(angles.sec));
+        this._Context.lineTo( this._clockFaceCenterX + (secArrowLenght-arrowTail) * Math.sin(angles.sec),
+            this._clockFaceCenterY - (secArrowLenght-arrowTail) * Math.cos(angles.sec) );
+        this._Context.stroke();
     }
 
-    function render() {
-        Context.clearRect(0,0,500,500);
-        drawClockFace();
-        drawNumbers();
-        drawElectronic();
-        drawArrows();
+    render() {
+        this._Context.clearRect(0,0,500,500);
+        this.drawClockFace();
+        this.drawNumbers();
+        this.drawElectronic();
+        this.drawArrows();
     }
 
-    function GetArrowsAngel() {
-        var angles = {};
-        var now = ClockModel.GetCurrentTime();
+    GetArrowsAngel() {
+        let angles = {};
+        let now = this._ClockModel.GetCurrentTime();
 
-        var secInMinute = 60;
-        var minInHour = 60;
+        let secInMinute = 60;
+        let minInHour = 60;
 
-        var hour = now.hours;
+        let hour = now.hours;
 
         angles['sec'] = now.seconds * 2 * Math.PI / secInMinute;
 
@@ -152,8 +154,5 @@ function TClockViewCanvas () {
 
         return angles;
     }
-
-
-
 }
 
