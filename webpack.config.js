47e980048b37f478044af1245c69cb07e6ed444e
DreamerQ <captainhope777@gmail.com>
2017-07-16 19:37:26 +0300

    updated config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
+// Для использования нужен Node.JS
+// Поставьте webpack:
+//   npm i -g webpack
+// Поставьте babel-loader:
+//   npm i babel-loader
+// Запустите его в директории с файлами:
+//   webpack
 
+module.exports = {
+    entry: './index',
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: "babel-loader" }
+        ]
+    }
+};
